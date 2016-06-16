5ec271485f4d4bea4db65f21da7bedb2011b2c62
cia48621793 <cia48621793@gmail.com>
2016-06-16 18:14:57 +0800

    Initialization

---
+++ MicroIoc.DotNet20/Hack.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections.Generic;
+
+namespace System.Collections.Generic
+{
+	interface IStructuralEquatable
+	{
+		bool Equals(Object that, IEqualityComparer comparer);
+		int GetHashCode(IEqualityComparer comparer);
+	}
+
+	interface IStructuralComparable
+	{
+		int CompareTo(object that, IComparer comparer);
+	}
+
+	[Serializable]
+	public class Tuple<T1, T2> : IStructuralEquatable, IStructuralComparable, IComparable, ITuple
+	{
+		public T1 Item1 { get; private set; }
+		public T2 Item2 { get; private set; }
+		int ITuple.Size { get { return 2; } }
+
+		public Tuple(T1 item1, T2 item2)
+		{
+			Item1 = item1;
+			Item2 = item2;
+		}
+
+		public override bool Equals(object that)
+		{
+			return ((IStructuralEquatable)this).Equals(that, EqualityComparer<object>.Default);
+		}
+
+		public override int GetHashCode()
+		{
+			return ((IStructuralEquatable)this).GetHashCode(EqualityComparer<object>.Default);
+		}
+
+		public override string ToString()
+		{
+			return "(" + ((ITuple)this).ToString() + ")";
+		}
+
+		bool IStructuralEquatable.Equals(object that, IEqualityComparer comparer)
+		{
+			if (that == null)
+			{
+				return false;
+			}
+			var thatTuple = that as Tuple<T1, T2>;
+
+			if (thatTuple == null)
+			{
+				return false;
+			}
+			return comparer.Equals(Item1, thatTuple.Item1)
+			&& comparer.Equals(Item2, thatTuple.Item2);
+		}
+
+		int IComparable.CompareTo(object that)
+		{
+			return ((IStructuralComparable)this).CompareTo(that, Comparer<object>.Default);
+		}
+
+		int IStructuralComparable.CompareTo(object that, IComparer comparer)
+		{
+			if (that == null)
+			{
+				return 1;
+			}
+			var thatTuple = that as Tuple<T1, T2>;
+			if (thatTuple == null)
+			{
+				return 1;
+			}
+			var c = 0;
+			c = comparer.Compare(Item1, thatTuple.Item1);
+			if (c != 0)
+			{
+				return c;
+			}
+			return comparer.Compare(Item2, thatTuple.Item2);
+		}
+
+		int IStructuralEquatable.GetHashCode(IEqualityComparer comparer)
+		{
+			return Tuple.CombineHashCodes(
+				comparer.GetHashCode(Item1),
+				comparer.GetHashCode(Item2)
+			);
+		}
+
+		string ITuple.ToString()
+		{
+			return string.Format("{0}, {1}", Item1, Item2);
+		}
+	}
+}
+
+namespace System
+{
+	public static class Tuple
+	{
+		internal static int CombineHashCodes(int h1, int h2)
+		{
+			return (((h1 << 5) + h1) ^ h2);
+		}
+		public static Tuple<T1, T2> Create<T1, T2>(T1 item1, T2 item2)
+		{
+			return new Tuple<T1, T2>(item1, item2);
+		}
+	}
+
+	interface ITuple
+	{
+		int Size { get; }
+		string ToString();
+	}
+}
---
+++ MicroIoc/MicroIoc.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace MicroIoc {
+    class ContainerTypeExistException : System.Exception  {
+
+    }
+
+    class ContainerBadEntryException : System.Exception {
+
+    }
+
+    public delegate TResult Func<TResult>();
+
+	class IocContainer : IEnumerable<KeyValuePair<Type, Tuple<object, Delegate>>>
+    {
+		private static IocContainer g_Instance;
+		public static IocContainer Instance {
+			get {
+				if (g_Instance == null) {
+					g_Instance = new IocContainer();
+				}
+				return g_Instance;
+			}
+		}
+
+		bool m_commit = false;
+		object m_lock = new object();
+        Dictionary<Type, Tuple<object, Delegate>> m_IocTable =
+            new Dictionary<Type, Tuple<object, Delegate>>();
+
+		public IocContainer() {
+		}
+
+		public IocContainer(params IocContainer[] others) {
+			foreach (var it in others) {
+				this.Import(it);
+			}
+		}
+
+        public IocContainer Register<T>(T input) where T : class {
+			lock (m_lock) {
+				if (m_commit) {
+					goto ret;
+				}
+				if (!m_IocTable.ContainsKey(typeof(T))) {
+					m_IocTable[typeof(T)] = Tuple.Create((object)input, (Delegate)null);
+				}
+				else {
+					throw new ContainerTypeExistException();
+				}
+			}
+		ret:
+            return this;
+        }
+
+        public IocContainer Register<T>(Func<T> input) where T : class {
+			lock (m_lock) {
+				if (m_commit) {
+					goto ret;
+				}
+				if (!m_IocTable.ContainsKey(typeof(T))) {
+					m_IocTable[typeof(T)] = Tuple.Create((object)null, (Delegate)input);
+				}
+				else {
+					throw new ContainerTypeExistException();
+				}
+			}
+		ret:
+            return this;
+        }
+
+		public IocContainer Register<T>() where T : class {
+			return Register<T>(Activator.CreateInstance<T>());
+		}
+
+        public T Get<T>() where T : class {
+			Tuple<object, Delegate> tuple;
+			lock (m_lock) {
+				tuple = m_IocTable[typeof(T)];
+			}
+            if (tuple == null) {
+                return default(T);
+            }
+            if (tuple.Item1 != null) {
+                return (T)tuple.Item1;
+            }
+            if (tuple.Item2 != null) {
+                return ((Func<T>)tuple.Item2)();
+            }
+            throw new ContainerBadEntryException();
+        }
+
+		public IocContainer Purge<T>() where T : class {
+			lock (m_lock) {
+				if (m_commit) {
+					goto circular;
+				}
+				m_IocTable.Remove(typeof(T));
+			}
+		circular:
+			return this;
+		}
+
+		public IocContainer Purge() {
+			lock (m_lock) {
+				if (m_commit) {
+					goto circular;
+				}
+				m_IocTable.Clear();
+			}
+		circular:
+			return this;
+		}
+
+		public IocContainer Lockdown() {
+			m_commit = true;
+			return this;
+		}
+
+		public IEnumerator<KeyValuePair<Type, Tuple<object, Delegate>>> GetEnumerator() {
+			lock (m_lock) {
+				return m_IocTable.GetEnumerator();
+			}
+		}
+
+		IEnumerator IEnumerable.GetEnumerator() {
+			return this.GetEnumerator();
+		}
+
+		public void Import(IocContainer other) {
+			lock (m_lock) {
+				foreach (var entry in other) {
+					m_IocTable.Add(entry.Key, entry.Value);
+				}
+			}
+		}
+	}
+}
